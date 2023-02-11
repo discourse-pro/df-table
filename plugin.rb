9e352686269b935f389de0f521b7d7a317ba9c24
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-11 11:58:58 +0300

    2.3.0: https://github.com/discourse-pro/df-core/issues/6

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: df-table
 # about: Support for tables in posts.
-# version: 2.2.0
+# version: 2.3.0
 # authors: Dmitry Fedyuk
 # url: https://discourse.pro/t/topic/38
 register_asset 'stylesheets/main.scss'
